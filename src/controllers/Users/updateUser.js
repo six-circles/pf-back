2b1a72c4e343661313b757acb6017e8066081cba
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-11 17:04:53 -0300

    Listo para conectar

--- src/controllers/Users/updateUser.js
+++ src/controllers/Users/updateUser.js
@@ -4,11 +4,12 @@ const updateUser = async (req, res) => {
   const { userID } = req.params;
   const { name, email, phone, password } = req.body;
   try {
+    const hash = await bcrypt.hash(password, 10);
     await User.findByIdAndUpdate(userID, {
       name: name,
       email: email,
       phone: phone,
-      password: password,
+      password: hash,
     });
     res.status(201).send({ message: "User updated" });
   } catch (error) {
