d24ffb74865e57225eb3149eeee0c7873de9f7de
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-11 12:24:06 -0300

    Cambiado las routes a index.js / develop

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const getProducts = require("./controllers/Products/getProducts");
-const postProduct = require("./controllers/Products/postProduct");
-const updateProducts = require("./controllers/Products/updateProducts");
-const postComment = require("./controllers/Comments/postComment");
-const getComments = require("./controllers/Comments/getComment");
+
 const mainRouter = require("./routes");
 
 const app = express();
@@ -14,12 +10,5 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 app.use(mainRouter);
-
-mainRouter.get("/product", getProducts);
-mainRouter.post("/product", postProduct);
-mainRouter.patch("/product/:id", updateProducts);
-
-mainRouter.post("/product/comments", postComment);
-mainRouter.get("/product/comments", getComments);
 
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,23 @@ const postProduct = require("../controllers/Products/postProduct");
 const postUser = require("../controllers/Users/postUsers");
 const updateUser = require("../controllers/Users/updateUser");
 const deleteUser = require("../controllers/Users/deleteUser");
+const getProducts = require("../controllers/Products/getProducts");
+const postProduct = require("../controllers/Products/postProduct");
+const updateProducts = require("../controllers/Products/updateProducts");
+const postComment = require("../controllers/Comments/postComment");
+const getComments = require("../controllers/Comments/getComment");
 
 const mainRouter = Router();
 
-mainRouter.post("/product", postProduct);
-
 mainRouter.post("/user", postUser);
-
 mainRouter.patch("/user/:userID", updateUser);
-
 mainRouter.delete("/user/:userID", deleteUser);
+
+mainRouter.get("/product", getProducts);
+mainRouter.post("/product", postProduct);
+mainRouter.patch("/product/:id", updateProducts);
+
+mainRouter.post("/product/comments", postComment);
+mainRouter.get("/product/comments", getComments);
 
 module.exports = mainRouter;
