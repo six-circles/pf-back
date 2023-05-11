a3127e8a6a282349cec69966bfb1ad9c09192674
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-11 18:13:12 -0300

    Arreglado bug ANSWERS

--- src/controllers/Products/updateProducts.js
+++ src/controllers/Products/updateProducts.js
@@ -2,18 +2,18 @@ const Product = require("../../models/Product");
 
 const updateProducts = async (req, res) => {
   const { productID } = req.params;
-  const { description, stock, price } = req.body;
+  const { description, stock, price, punctuations } = req.body;
 
   try {
-    if (!description || !stock || !price) {
+    if (!description || !stock || !price || !punctuations) {
       throw Error("Faltan datos");
     }
     const putProducts = await Product.findByIdAndUpdate(productID, {
       price: price,
       description: description,
       stock: stock,
+      punctuations: punctuations,
     });
-    console.log(putProducts);
     res.status(200).json("Producto modificado");
   } catch (error) {
     res.status(404).json({ error: error.message });
