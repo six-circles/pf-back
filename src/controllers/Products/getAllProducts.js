4787fffe25e3805c65300cf94845abe68e493cc4
MatiiChoque <choquematia@gmail.com>
2023-05-12 17:35:08 -0300

    modificando controllador allProducts para recibir title por query (inconvenientes con el "filter")/puliendo-develop

--- src/controllers/Products/getAllProducts.js
+++ src/controllers/Products/getAllProducts.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/Product");
 
 const getAllProducts = async (req, res) => {
+  const { title } = req.query;
   try {
     const products = await Product.find()
       .populate("comments", {
@@ -13,7 +14,36 @@ const getAllProducts = async (req, res) => {
         __v: 0,
         _id: 0,
       });
-    res.status(200).json(products);
+
+    if (title) {
+      const minusTitle = title.toLowerCase();
+      const myArray = [];
+      //todo PROBLEMAS PARA FILTRAR EL ARRAY DE PRODUCTOS EN BASE AL TITULO
+      //todo LA LINEA DE COMPARACION ENTRE AMBAS MINUSCULAS FUNCIONA BIEN, EL PROBLEMA ES EL "FILTER", DEVUELVE ARRAY VACIO.
+      // const productsName = products.filter((prod) => {
+      //   prod.title.toLowerCase().includes(minusTitle);
+      // });
+      console.log(products[0].title.toLowerCase().includes(minusTitle));
+
+      for (let prod of products) {
+        let include = products[prod].title.toLowerCase().includes(minusTitle);
+        if (include) {
+          myArray.push(products[prod]);
+        }
+      }
+      console.log({ res: myArray });
+      // console.log(
+      //   products.filter((prod) => {
+      //     prod.title.toLowerCase().includes(minusTitle);
+      //   })
+      // );
+
+      productsName
+        ? res.status(200).json(productsName)
+        : res.status(404).send("Product is not found.");
+    } else {
+      res.status(200).json(products);
+    }
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
