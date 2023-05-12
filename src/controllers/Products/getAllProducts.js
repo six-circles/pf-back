76e63128fe01a6d71356a342d360399bc8058e80
matiichoque <choquematia@gmail.com>
2023-05-12 20:54:33 -0300

    controlador get allProducts terminado recibe busqueda por "title" /puliendo-develop

--- src/controllers/Products/getAllProducts.js
+++ src/controllers/Products/getAllProducts.js
@@ -17,28 +17,14 @@ const getAllProducts = async (req, res) => {
 
     if (title) {
       const minusTitle = title.toLowerCase();
-      const myArray = [];
-      //todo PROBLEMAS PARA FILTRAR EL ARRAY DE PRODUCTOS EN BASE AL TITULO
-      //todo LA LINEA DE COMPARACION ENTRE AMBAS MINUSCULAS FUNCIONA BIEN, EL PROBLEMA ES EL "FILTER", DEVUELVE ARRAY VACIO.
-      // const productsName = products.filter((prod) => {
-      //   prod.title.toLowerCase().includes(minusTitle);
-      // });
-      console.log(products[0].title.toLowerCase().includes(minusTitle));
-
-      for (let prod of products) {
-        let include = products[prod].title.toLowerCase().includes(minusTitle);
+      const productsName = [];
+      for (let i=0; i<products.length;i++) {
+        let include = products[i].title.toLowerCase().includes(minusTitle);
         if (include) {
-          myArray.push(products[prod]);
+          productsName.push(products[i]);
         }
-      }
-      console.log({ res: myArray });
-      // console.log(
-      //   products.filter((prod) => {
-      //     prod.title.toLowerCase().includes(minusTitle);
-      //   })
-      // );
-
-      productsName
+      };
+      productsName.length
         ? res.status(200).json(productsName)
         : res.status(404).send("Product is not found.");
     } else {
