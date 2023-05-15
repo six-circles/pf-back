0df7b1140c0a643150009cf3dced77d25aeebebe
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-15 19:24:50 -0300

    Codigo despues de limpiar y arreglado el bug de combinacion

--- src/controllers/Products/getAllProducts.js
+++ src/controllers/Products/getAllProducts.js
@@ -1,93 +1,37 @@
 const Product = require("../../models/Product");
-
 const getAllProducts = async (req, res) => {
   const { title, orderPrice, orderTitle, orderPunctuations, index1, index2 } =
     req.query;
   try {
-    const products = await Product.find()
-      .populate("comments", {
-        products: 0,
-        __v: 0,
-        _id: 0,
-      })
-      .populate("questions", {
-        products: 0,
-        __v: 0,
-        _id: 0,
-      })
+    let query = Product.find();
+
+    if (title) {
+      const minusTitle = title.toLowerCase();
+      query = query.where("title").regex(new RegExp(minusTitle, "i"));
+    }
+
+    if (orderPrice) {
+      query = query.sort({ price: orderPrice });
+    } else if (orderTitle) {
+      query = query.sort({ title: orderTitle });
+    } else if (orderPunctuations) {
+      query = query.sort({ punctuations: orderPunctuations });
+    }
+
+    const products = await query
+      .populate("comments", { products: 0, __v: 0, _id: 0 })
+      .populate("questions", { products: 0, __v: 0, _id: 0 })
       .skip(index1)
       .limit(index2);
 
-    const productsPaginated = await Product.find().skip(index1).limit(index2);
-    console.log(productsPaginated);
-
-    if (orderPrice) {
-      const productsOrdered = await Product.find()
-        .sort({ price: orderPrice })
-        .populate("comments", {
-          products: 0,
-          __v: 0,
-          _id: 0,
-        })
-        .populate("questions", {
-          products: 0,
-          __v: 0,
-          _id: 0,
-        })
-        .skip(index1)
-        .limit(index2);
-      res.status(200).json(productsOrdered);
-    } else if (orderTitle) {
-      const productsOrdered = await Product.find()
-        .sort({ title: orderTitle })
-        .populate("comments", {
-          products: 0,
-          __v: 0,
-          _id: 0,
-        })
-        .populate("questions", {
-          products: 0,
-          __v: 0,
-          _id: 0,
-        })
-        .skip(index1)
-        .limit(index2);
-      res.status(200).json(productsOrdered);
-    } else if (orderPunctuations) {
-      const productsOrdered = await Product.find()
-        .sort({
-          punctuations: orderPunctuations,
-        })
-        .populate("comments", {
-          products: 0,
-          __v: 0,
-          _id: 0,
-        })
-        .populate("questions", {
-          products: 0,
-          __v: 0,
-          _id: 0,
-        })
-        .skip(index1)
-        .limit(index2);
-      res.status(200).json(productsOrdered);
-    } else if (title) {
-      const minusTitle = title.toLowerCase();
-      const productsName = [];
-      for (let i = 0; i < products.length; i++) {
-        let include = products[i].title.toLowerCase().includes(minusTitle);
-        if (include) {
-          productsName.push(products[i]);
-        }
-      }
-      productsName.length
-        ? res.status(200).json(productsName)
-        : res.status(404).send("Product is not found.");
-    } else {
+    if (products.length) {
       res.status(200).json(products);
+    } else {
+      res.status(404).send("Product is not found.");
     }
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
 };
+
 module.exports = getAllProducts;
