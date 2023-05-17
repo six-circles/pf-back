ccadae1803d36f5925ed187d6b62b079eed414db
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-17 15:10:06 -0300

    Listo update and delete User

--- src/controllers/Users/updateUser.js
+++ src/controllers/Users/updateUser.js
@@ -1,11 +1,15 @@
 const User = require("../../models/User");
 
 const updateUser = async (req, res) => {
-  const { userID } = req.params;
+  const { token } = req.params;
   const { name, email, phone, password } = req.body;
   try {
+    const userId = jwt.verify(token, process.env.SECRET_KEY_JWT);
+    if (!userId) throw Error("No estas logueado");
+    const user = await User.findById(userId.userId);
+
     const hash = await bcrypt.hash(password, 10);
-    await User.findByIdAndUpdate(userID, {
+    await User.findByIdAndUpdate(user._id, {
       name: name,
       email: email,
       phone: phone,
