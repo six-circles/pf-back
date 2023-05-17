8b71f6c0f1c150cc700025c7ae99c50c94c8b535
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-17 16:58:21 -0300

    Probando Google Auth sin passport

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+require("dotenv").config();
+const axios = require("axios");
 const postUser = require("../controllers/Users/postUsers");
 const updateUser = require("../controllers/Users/updateUser");
 const deleteUser = require("../controllers/Users/deleteUser");
@@ -74,5 +76,53 @@ mainRouter.delete("/delivery/:id", /*checkLogin ,*/ deleteDelivery);
 mainRouter.get("/:userID/shoppingCart", getSC);
 mainRouter.post("/user/shoppingCart", postSC);
 mainRouter.delete("/:userID/shoppingCart/:productID", deleteSC);
+
+mainRouter.get("/auth/google", (req, res) => {
+  // Redirige al usuario a la página de inicio de sesión de Google
+  const scope = ["profile", "email"]; // Define los alcances que necesitas
+  const redirectUri = "http://localhost:3001/auth/google/callback";
+  const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
+    process.env.GOOGLE_CLIENT_ID
+  }&redirect_uri=${redirectUri}&response_type=code&scope=${scope.join("+")}`;
+  res.redirect(authUrl);
+});
+
+mainRouter.get("/auth/google/callback", async (req, res) => {
+  const code = req.query.code; // Obtiene el código de autorización de la URL de retorno
+  const redirectUri = "http://localhost:3001/auth/google/callback";
+
+  // Intercambia el código de autorización por un token de acceso
+  const tokenUrl = "https://oauth2.googleapis.com/token";
+  const data = {
+    code,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    redirect_uri: redirectUri,
+    grant_type: "authorization_code",
+  };
+
+  try {
+    const response = await axios.post(tokenUrl, data);
+    const accessToken = response.data.access_token;
+
+    // Utiliza el token de acceso para realizar operaciones con la API de Google, como obtener información del usuario
+    const userInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo";
+    const userResponse = await axios.get(userInfoUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const userData = userResponse.data;
+
+    // Aquí puedes hacer lo que necesites con los datos del usuario obtenidos de Google
+
+    // Redirige o responde con los datos del usuario según tu lógica de aplicación
+    res.status(200).json({ message: "Login By Google successfully" });
+  } catch (error) {
+    // Manejo de errores
+    console.error(error);
+    res.status(500).send("Error en la autenticación con Google");
+  }
+});
 
 module.exports = mainRouter;
