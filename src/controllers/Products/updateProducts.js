3fd80d3414d866149aa4f210688996b0d75fd3fe
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-18 13:18:21 -0300

    Arreglado el updateUser, y agregado updateUser

--- src/controllers/Products/updateProducts.js
+++ src/controllers/Products/updateProducts.js
@@ -2,7 +2,7 @@ const Product = require("../../models/Product");
 
 const updateProducts = async (req, res) => {
   const { productID } = req.params;
-  const { description, stock, price, enable } = req.body;
+  const { description, stock, price, enable, title } = req.body;
 
   try {
     const putProducts = await Product.findByIdAndUpdate(productID, {
@@ -10,6 +10,7 @@ const updateProducts = async (req, res) => {
       description: description,
       stock: stock,
       enable: enable,
+      title: title,
     });
     res.status(200).json("Producto modificado");
   } catch (error) {
