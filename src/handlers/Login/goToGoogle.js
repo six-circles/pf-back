7fec1f2a81b958c023ccafa4a030af157f108053
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-21 15:22:01 -0300

    Agregado redirectUriGoogle como variable de entorno

--- src/handlers/Login/goToGoogle.js
+++ src/handlers/Login/goToGoogle.js
@@ -3,11 +3,11 @@ const axios = require("axios");
 const goToGoogle = (req, res) => {
   // Redirige al usuario a la página de inicio de sesión de Google
   const scope = ["profile", "email"]; // Define los alcances que necesitas
-  const redirectUri =
-    "pf-back-sixcirlces-production.up.railway.app/auth/google/callback";
   const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
     process.env.GOOGLE_CLIENT_ID
-  }&redirect_uri=${redirectUri}&response_type=code&scope=${scope.join("+")}`;
+  }&redirect_uri=${
+    process.env.redirectUriGoogle
+  }&response_type=code&scope=${scope.join("+")}`;
   res.redirect(authUrl);
 };
 module.exports = goToGoogle;
