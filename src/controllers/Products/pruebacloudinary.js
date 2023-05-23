137cbea6dceb421a1ee8d90ab2573d889a68e35d
MatiiChoque <choquematia@gmail.com>
2023-05-23 18:20:01 -0300

    adaptanto nuevo post product con cloudinary

--- src/controllers/Products/pruebacloudinary.js
+++ src/controllers/Products/pruebacloudinary.js
@@ -4,15 +4,58 @@ const jwt = require("jsonwebtoken");
 const cloudinary = require("../../config/cloudinary");
 
 const pruebacloudinary = async (req, res) => {
+  const {
+    title,
+    description,
+    stock,
+    price,
+    condition,
+    token,
+    category,
+    moreCharacteristics,
+  } = req.body;
   console.log(req.body);
   console.log(req.file);
   try {
-    const newProduct = {};
+    if (
+      !title ||
+      !description ||
+      !stock ||
+      !price ||
+      !condition ||
+      !token ||
+      !category
+    )
+      throw Error("Faltan datos");
+    if (
+      category !== "Technology" &&
+      category !== "Furniture" &&
+      category !== "Indumentary" &&
+      category !== "Others"
+    ) {
+      throw Error("Invalid Category");
+    }
+
+    const userId = jwt.verify(token, process.env.SECRET_KEY_JWT);
+
+    const user = await User.findById(userId.userId);
+
+    const newProduct = await Product.create({
+      title: title,
+      description: description,
+      stock: stock,
+      price: price,
+      condition: condition,
+      user: user._id,
+      category: category,
+      moreCharacteristics: moreCharacteristics,
+    });
 
     try {
       const result = await cloudinary.uploader.upload(req.file.path);
-      const objImg = { id: result.public_id, url: result.secure_url };
-      newProduct.image = objImg;
+      const objImg = { cloudinaryID: result.public_id, url: result.secure_url };
+      newProduct.image.push(objImg);
+      newProduct.save();
     } catch (error) {
       console.log(error);
     }
