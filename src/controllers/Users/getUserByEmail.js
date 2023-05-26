85455cdcb50a9e1adfb93466f6ed1abcc558bc89
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-26 15:52:35 -0300

    Agrgeando propiedades admin y mejorando getUserByEmail

--- src/controllers/Users/getUserByEmail.js
+++ src/controllers/Users/getUserByEmail.js
@@ -1,9 +1,24 @@
 const User = require("../../models/User");
+const Product = require("../../models/Product");
 
 const getUserByEmail = async (req, res) => {
   const { email } = req.query;
   try {
     const user = await User.findOne({ email: email });
+
+    const products = await Product.find({ user: user._id });
+
+    user.products = products;
+
+    let suma = 0;
+
+    if (products.length) {
+      for (element of products) {
+        suma += element.punctuations;
+      }
+      promedio = suma / products.length;
+      user.punctuation = promedio;
+    }
 
     res.status(200).json(user);
   } catch (error) {
