cccb19c5d535a9bc79591689f7442c4d4d6b0727
MatiiChoque <choquematia@gmail.com>
2023-05-28 16:13:38 -0300

    enable user, enable products

--- src/controllers/Users/enableUserById.js
+++ src/controllers/Users/enableUserById.js
@@ -1,17 +1,27 @@
 const User = require("../../models/User");
+const Product = require("../../models/Product");
 
 const deleteUserById = async (req, res) => {
   const { id } = req.query;
   console.log(req.query);
   try {
     const user = await User.findById(id);
+    const products = await Product.find({ user: user });
     if (user.enable === false) {
       user.enable = true;
       user.save();
+      for (let element of products) {
+        element.enable = true;
+      }
+      products.save();
       res.status(201).send({ message: "Unlocked user" });
     } else {
       user.enable = false;
       user.save();
+      for (let element of products) {
+        element.enable = false;
+      }
+      products.save();
       res.status(201).send({ message: "User blocked" });
     }
   } catch (err) {
