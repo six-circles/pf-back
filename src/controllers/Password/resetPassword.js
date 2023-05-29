7346ce0e63149ee4de8c51a4f8d20683b039eac3
MatiiChoque <choquematia@gmail.com>
2023-05-29 19:19:02 -0300

    update

--- src/controllers/Password/resetPassword.js
+++ src/controllers/Password/resetPassword.js
@@ -9,7 +9,7 @@ const resetPassword = async (req, res) => {
   const secret = secretKey + user.password;
   try {
     const payload = jwt.verify(token, secret);
-    res.status(200).json({ message: user.email });
+    res.status(200).json({ user: user.name, email: payload.email });
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
