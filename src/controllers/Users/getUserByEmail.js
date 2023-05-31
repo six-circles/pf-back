a35005b22b2aff18ceda0154f0ff4f2767e10bf7
MatiiChoque <choquematia@gmail.com>
2023-05-30 21:01:53 -0300

    cantpuntuactions

--- src/controllers/Users/getUserByEmail.js
+++ src/controllers/Users/getUserByEmail.js
@@ -11,16 +11,18 @@ const getUserByEmail = async (req, res) => {
     user.products = products;
 
     let suma = 0;
+    let cantPuntuaciones = 0;
 
     if (products.length) {
       for (element of products) {
         suma += element.punctuations;
+        cantPuntuaciones += element.comments.length;
       }
       promedio = suma / products.length;
       user.punctuation = promedio;
     }
 
-    res.status(200).json(user);
+    res.status(200).json({ user: user, cantPuntuaciones: cantPuntuaciones });
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
