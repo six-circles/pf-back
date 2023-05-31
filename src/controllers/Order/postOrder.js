8bbccedb4dfce3951e6e7553a6053328aaf49985
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-31 15:45:50 -0300

    Listo validacion de stock

--- src/controllers/Order/postOrder.js
+++ src/controllers/Order/postOrder.js
@@ -40,7 +40,7 @@ const postOrder = async (req, res) => {
     for (const element of productSC) {
       await Product.findOneAndUpdate(
         { _id: element._id },
-        { $inc: { stock: -1 } }
+        { $inc: { stock: -1 }, $set: { enable: element.stock > 1 } }
       );
     }
     res
